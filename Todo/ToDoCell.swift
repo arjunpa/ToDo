b8deccd9bde781b9486ee978d168ea4d284c183f
Arjun P A <arjun.pa2@gmail.com>
2020-06-25 01:17:40 +0530

    Added ToDoCell

---
+++ Todo/ToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoCell.swift
+//  Todo
+//
+//  Created by Arjun P A on 24/06/20.
+//  Copyright © 2020 Arjun P A. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
